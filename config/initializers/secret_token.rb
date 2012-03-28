c492b3ddc08281c9bb0a5f4f50eb75927d780dd9
Joshua D. Harris <k0901861@kingston.ac.uk>
2012-03-28 13:51:44 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsEms::Application.config.secret_token = '9c6218c68e08db8b19675664619d854df3ff891a87cdbb69cc13d04bf5d97edd3384d3c956e8bac39c2fa507769e54868f8ccac219d1251fe460bd14ed1cc256'
